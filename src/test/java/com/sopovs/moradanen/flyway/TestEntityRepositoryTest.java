8fcac014faf1d5481c89d3b2cd3d6d9a04dbb23c
wololock <szymon.stepniak@gmail.com>
2014-10-07 12:38:45 +0200

    config file initializer added to integration test

--- src/test/java/com/sopovs/moradanen/flyway/TestEntityRepositoryTest.java
+++ src/test/java/com/sopovs/moradanen/flyway/TestEntityRepositoryTest.java
@@ -5,6 +5,7 @@ import static org.junit.Assert.assertNotNull;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.ConfigFileApplicationContextInitializer;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
@@ -12,7 +13,7 @@ import com.sopovs.moradanen.flyway.domain.TestEntity;
 import com.sopovs.moradanen.flyway.repositories.TestEntityRepository;
 
 @RunWith(SpringJUnit4ClassRunner.class)
-@ContextConfiguration(classes = FlywaySpringBootTestApplication.class)
+@ContextConfiguration(classes = FlywaySpringBootTestApplication.class, initializers = ConfigFileApplicationContextInitializer.class)
 public class TestEntityRepositoryTest {
 
 	@Autowired
